4779c4f1ce49f2563057dc21ff2eefce01a8765b
Michael Stafford <michaelstafford808@gmail.com>
2022-03-17 17:48:19 -0600

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,9 +1,12 @@
+let timerID = setInterval(addCounter, 1000)
+
 document.addEventListener('DOMContentLoaded', () => {
     //after doing so, find a method to increase the counter.innerHTML by 1 every second
-    setInterval(addCounter, 1000)
+    timerID;
     plusButton()
     minusButton()
     heartButton()
+    pauseButton()
 })
 
 //get the timer to start
@@ -53,29 +56,60 @@ function addCounter(){
     const heartButton = document.getElementById('heart')
     //what next? add an event listener that will listen to a click of that heart
     //that will run a callback or an anonymous arrow function 
+
+    let increaseLikes = 0
+
     heartButton.addEventListener('click', () => {
         const likeHolder = document.querySelector('.likes')
         console.log(likeHolder)
-
+        ++increaseLikes
         const counter = document.getElementById('counter')
         //which will then get access to the ul of the likes by its class ="likes" by using querySelector 
         //create li tag to store the comment 
+
+        const likeLi = document.getElementById(`${counter.innerHTML}`)
+        console.log("LIKE", likeLi)
+
+
+
+
+        if(likeLi != null){
+            likeLi.innerText = `${counter.innerHTML} has been liked ${increaseLikes} times`
+            // ++increaseLikes
+        } else {
+        increaseLikes = 1
         const likeComment = document.createElement('li')
-        //set data attribute to the current value of the counter 
+        likeComment.id = counter.innerHTML
+
         likeComment.dataset.num = `${counter.innerHTML}`
-        likeComment.innerText = `${counter.innerHTML} has been liked times`
+        likeComment.innerText = `${counter.innerHTML} has been liked ${increaseLikes} times`
 
-        //create and add span tag to the li which is likeComment
-        const span = document.createElement('span')
-        
-
-
-
-        likeComment.append(span)
         likeHolder.append(likeComment)
 
+
+        }
 
     })
 }
 
-//what's the next value that we are trying to interpolate??
+//pause the counter 
+//disable all buttons except the pause button
+//switch the label on the button from "pause" to "resume"
+
+//what do do first?
+//get access to the pause button by the id 
+//add click event listener which will change the inner text of the pause button to resume 
+
+function pauseButton(){
+    const pause = document.getElementById('pause')
+    pause.addEventListener('click', () => {
+        if(pause.innerText === "pause"){
+            pause.innerText = "resume"
+            clearInterval(timerID)
+        } else {
+            pause.innerText = "pause"
+            setTimeout(addCounter,1000)
+            
+        }
+    })
+}
